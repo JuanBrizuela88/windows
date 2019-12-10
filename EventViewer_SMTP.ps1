2422fe03fb39fc9385486a5fff6fb3467c17f1aa
Juan Brizuela <58746656+JuanBrizuela88@users.noreply.github.com>
2019-12-10 16:44:13 -0600

    Create EventViewer_SMTP.ps1

---
+++ EventViewer_SMTP.ps1
@@ -0,0 +1,32 @@
+#Script para enviar logs por medio de correo
+#Creado por Juan Brizuela
+<#
+IDs de Evento:
+Sesión Windows Fallida. Log: Security , ID 4625
+Sesion SQL fallida Log: MSSQL , ID 18456
+Instalacion Aplicacion. Log: Application , ID 11707
+Cambios en firewall Log: Security, ID 4947
+Creacion de Usuario Log: Security, ID 4720
+Bloqueos de Firewall Log: Security, ID 5152
+Eliminacion de Logs Log: Security, ID, 1102
+#>
+
+$EmailFrom = "correo@dominio.com"
+
+$EmailTo = "correo@dominio.com"
+
+$Subject = "Inicio de sesion/Malware Detectado/OTRO"
+
+$LogText = Get-WinEvent -FilterHashTable @{LogName='$Log';ID='$numID'} -MaxEvents 1 | Format-List > $archivo.txt
+
+$Body = "$Body = Get-Content -Path $archivo.txt | out-string"
+
+$SMTPServer = "$ServidorSMTP"
+
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+
+$SMTPClient.EnableSsl = $true
+
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("correo@dominio.com", "password");
+
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
